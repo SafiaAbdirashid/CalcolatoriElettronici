d6a1704df96c79bdba2cd2d37e4e75412d58c99e
SafiaAbdirashid <114006045+SafiaAbdirashid@users.noreply.github.com>
2024-06-05 12:08:54 +0200

    Create main.asm

---
+++ laboratorio 3/esercizio 4/main.asm
@@ -0,0 +1,28 @@
+.data
+            DIM = 3
+			
+.text
+.globl main
+.ent main
+
+main:
+        add $t1, $0, $0  # variabile somma
+		    add $t2, $0, $0  # contatore
+		
+		    loop:
+		        li $v0, 5
+				    syscall
+				    add $t0, $v0, $0
+				    add $t1, $t1, $t0
+				    addi $t2, 1
+				    bne $t2, DIM, loop
+				
+		div $t1, $t1, $t2
+		
+		li $v0, 1
+		add $a0, $t1, $0
+		syscall
+		
+		li $v0, 10
+		syscall
+		.end main
